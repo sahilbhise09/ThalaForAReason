57eda5a84a5a5678eb2dc2888a01616de73f724a
sahilbhise09 <sahilbhise09@gmail.com>
2023-12-23 01:20:49 +0530

    updated

--- script.js
+++ script.js
@@ -5,22 +5,23 @@ function getValue() {
     // Check if the length of the input is 7
     if (ans.length == 7) {
         console.log(`${ans} has 7 letters thala for a reason`);
-        document.getElementById('output').innerHTML = `${ans} has 7 letters thala for a reason`;
-        document.getElementById('dhoni').innerHTML = '<img src="bolejokoyal.gif" alt="Thala Gif">'
-        document.getElementById('audio').innerHTML = '<audio src="Thala.mp3" autoplay loop></audio>';
+        document.getElementById('output').innerHTML = `${ans} has 7 letters Thala for a reason`;
+        document.getElementById('dhoni').innerHTML = '<img src="./Assets/images/bolejokoyal.gif" alt="Thala Gif">'
+        document.getElementById('audio').innerHTML = '<audio src="./Assets/music/Thala.mp3" autoplay loop></audio>';
     } else if (sums(ans) == 7) {
         // Check if the sum of digits is 7
         console.log(`${ans} thala for a reason`);
-        document.getElementById('output').innerHTML = `${ans} thala for a reason`;
-        document.getElementById('dhoni').innerHTML = '<img src="bolejokoyal.gif" alt="Thala Gif">'
-        document.getElementById('audio').innerHTML = '<audio src="Thala.mp3" autoplay loop></audio>';
+        document.getElementById('output').innerHTML = `${ans} Thala for a reason`;
+        document.getElementById('dhoni').innerHTML = '<img src="./Assets/images/bolejokoyal.gif" alt="Thala Gif">'
+        document.getElementById('audio').innerHTML = '<audio src="./Assets/music/Thala.mp3" autoplay loop></audio>';
     } else {
         console.log(`${ans} Not Thala for a reason`);
-        document.getElementById('dhoni').innerHTML = '<img src="moye.gif" alt="moye Gif" width="300px">'
+        document.getElementById('dhoni').innerHTML = '<img src="./Assets/images/moye.gif" alt="moye Gif" width="300px">'
         document.getElementById('output').innerHTML = `${ans} Not Thala for a reason`;
-        document.getElementById('audio').innerHTML = '<audio src="moye.mp3" autoplay loop></audio>';
+        document.getElementById('audio').innerHTML = '<audio src="./Assets/music/moye.mp3" autoplay loop></audio>';
     }
 }
+
 function sums(ans) {
     let convnum = parseInt(ans);
     let sum = 0;
